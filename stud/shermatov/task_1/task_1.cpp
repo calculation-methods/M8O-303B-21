a2646980bdfcd5f6af7c04b1af1a299331d86403
HaPPyDutCHoGGG <hdh.egor.shermatov@gmail.com>
2024-05-31 00:30:18 +0300

    lab1.1 - done

---
+++ stud/shermatov/task_1/task_1.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <tuple>
+
+
+using namespace std;
+
+typedef vector<vector<double>> Matrix;
+typedef vector<double> Vector;
+
+void printMatrix(const Matrix &mat) {
+    for (const auto &row : mat) {
+        for (double val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
+void printVector(const Vector &vec) {
+    for (double val : vec) {
+        cout << val << " ";
+    }
+    cout << endl;
+}
+
+double determinant(const Matrix &U) {
+    double det = 1.0;
+    for (int i = 0; i < U.size(); i++) {
+        det *= U[i][i]; // Умножаем диагональные элементы матрицы U
+    }
+    return det;
+}
+
+pair<Matrix, Matrix> luDecomposition(const Matrix &A) {
+    int n = A.size();
+    Matrix L(n, vector<double>(n, 0.0));
+    Matrix U(n, vector<double>(n, 0.0));
+
+    for (int i = 0; i < n; i++) {
+        L[i][i] = 1.0;
+
+        for (int k = i; k < n; k++) {
+            double sum = 0;
+            for (int j = 0; j < i; j++) {
+                sum += L[i][j] * U[j][k];
+            }
+            U[i][k] = A[i][k] - sum;
+        }
+
+        for (int k = i; k < n; k++) {
+            double sum = 0;
+            for (int j = 0; j < i; j++) {
+                sum += L[k][j] * U[j][i];
+            }
+            L[k][i] = (A[k][i] - sum) / U[i][i];
+        }
+    }
+
+    return make_pair(L, U);
+}
+
+Vector solveLinearEquationSystem(const Matrix &A, const Vector &b) {
+    int n = A.size();
+    Vector x(n);
+    Matrix L, U;
+    tie(L, U) = luDecomposition(A);
+
+    // Solve Ly = b
+    Vector y(n);
+    for (int i = 0; i < n; i++) {
+        double sum = 0;
+        for (int j = 0; j < i; j++) {
+            sum += L[i][j] * y[j];
+        }
+        y[i] = (b[i] - sum) / L[i][i];
+    }
+
+    // Solve Ux = y
+    for (int i = n - 1; i >= 0; i--) {
+        double sum = 0;
+        for (int j = i + 1; j < n; j++) {
+            sum += U[i][j] * x[j];
+        }
+        x[i] = (y[i] - sum) / U[i][i];
+    }
+
+    return x;
+}
+
+Matrix inverseMatrix(const Matrix &A) {
+    int n = A.size();
+    Matrix inverse(n, Vector(n, 0.0));
+    Matrix L, U;
+    tie(L, U) = luDecomposition(A);
+
+    Matrix identity(n, Vector(n, 0.0));
+    for (int i = 0; i < n; i++) {
+        identity[i][i] = 1.0;
+        Vector b = identity[i];
+        Vector x = solveLinearEquationSystem(A, b);
+        for (int j = 0; j < n; j++) {
+            inverse[j][i] = x[j];
+        }
+    }
+
+    return inverse;
+}
+
+int main() {
+    Matrix A = {{1, 4, -9, 7},
+                 {2, -2, -2, 3},
+                 {-1, 3, -9, -1},
+                 {-5, 2, 2, 1}};
+    Vector b = {-67, -57, -26, 52};
+
+    Vector x = solveLinearEquationSystem(A, b);
+
+    cout << "Solution x:" << endl;
+    printVector(x);
+
+    Matrix A_inv = inverseMatrix(A);
+
+    cout << "Inverse matrix A_inv:" << endl;
+    printMatrix(A_inv);
+    
+    double det = determinant(A);
+    cout << "Determinant:" << det << endl;
+
+    return 0;
+}
